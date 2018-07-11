6be372504ea8fde586f20cdf48d15e6c30a52c87
howardmann <howardmann27@gmail.com>
2018-07-11 20:55:08 +1000

    Add example of side effect helper

---
+++ fp-light/sideffect/1.js
@@ -0,0 +1,37 @@
+// ## Calls the function with arg and returns the original arg
+// This is helpful when using promises and wanting to call a sideffect but then 
+// pass the original result further down the chain
+
+// Util helpers to DRY code
+let sideEffect = fn => a => {
+  fn(a) // process side-effects with arg
+  return a // pass the arg further
+}
+
+let fetchAll = () => Promise.resolve({name: 'Howie', looks: 'handsome', id: '123'})
+let postAPI = (id) => Promise.resolve({status: 'ok', id:'123'})
+let fetchAge = (id) => Promise.resolve({age: 18, id:'123'})
+
+// 1. Before using helper
+// Problems. not clear where side effects are occuring
+fetchAll()
+  .then(result => {
+    // There are 2 side effects here but its not clear
+    console.log(`Name returned: ${result.name}`)
+    return postAPI(result.id)
+  })
+  .then(result => {    
+    let id = result.id
+    return fetchAge(id)
+  })
+  .then(result => {
+    console.log(`Age is: ${result.age}`);
+  })
+
+// 2. Using helper
+// Clearer where sideffects are occuring
+fetchAll()
+  .then(sideEffect(o => console.log(`Name returned: ${o.name}`)))
+  .then(sideEffect(o => postAPI(o.id)))
+  .then(o => fetchAge(o.id))
+  .then(o => console.log(`Age is: ${o.age}`))
